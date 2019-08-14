b44ff6caa0f4c07543fd9f33b666045ecc393a47
mfv-computer-0027 <mfv-computer-0027@mfv-computer-0027.local>
2019-08-14 15:54:30 +0700

    Update ECS intergrate IAM

--- ecs_datadog_iam.tf
+++ ecs_datadog_iam.tf
@@ -1,45 +1,50 @@
-resource "aws_iam_role" "ecs-datadog-role" {
-  name = "${var.env}-${var.identifier}-jenkins-ecs-datadog-role"
-  assume_role_policy = "${data.aws_iam_policy_document.ecs-task-role-assume.json}"
+variable "datadog_aws_integration_external_id" {
+  default = "0e21499d3743404bbd8ab31b05bce792"
+  description = ""
 }
 
-resource "aws_iam_role_policy_attachment" "ecs-datadog" {
-  role = "${aws_iam_role.ecs-datadog-role.name}"
-  policy_arn = "${aws_iam_policy.ecs-datadog-role-policy.arn}"
-}
-
-resource "aws_iam_policy" "ecs-datadog-role-policy" {
-  name = "${var.env}-${var.identifier}-datadog-agent-ecs-iam-role-policy"
-  policy = "${data.aws_iam_policy_document.ecs-datadog-role.json}"
-}
-
-data "aws_iam_policy_document" "ecs-datadog-role" {
-
+data "aws_iam_policy_document" "datadog_aws_integration_assume_role" {
   statement {
-    sid = "AllowDatadogToReadECSMetrics"
-    effect = "Allow"
-    actions = [
-      "ecs:RegisterContainerInstance",
-      "ecs:DeregisterContainerInstance",
-      "ecs:DiscoverPollEndpoint",
-      "ecs:Submit*",
-      "ecs:Poll",
-      "ecs:StartTask",
-      "ecs:StartTelemetrySession"
-    ]
-    resources = [ "*" ]
-  }
+    actions = ["sts:AssumeRole"]
 
-}
-
-data "aws_iam_policy_document" "ecs-task-role-assume" {
-  statement {
-    effect = "Allow"
-    actions = [ "sts:AssumeRole" ]
     principals {
-      type = "Service"
-      identifiers = [ "ecs-tasks.amazonaws.com" ]
+      type = "AWS"
+      identifiers = ["arn:aws:iam::464622532012:root"]
+    }
+    condition {
+      test = "StringEquals"
+      variable = "sts:ExternalId"
+
+      values = [
+        "${var.datadog_aws_integration_external_id}"
+      ]
     }
   }
 }
 
+data "aws_iam_policy_document" "datadog_aws_integration" {
+  statement {
+    actions = [
+      "ecs:Describe*",
+      "ecs:List*"
+    ]
+    effect = "Allow"
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "datadog_aws_integration" {
+  name = "DatadogAWSIntegrationPolicy"
+  policy = "${data.aws_iam_policy_document.datadog_aws_integration.json}"
+}
+
+resource "aws_iam_role" "datadog_aws_integration" {
+  name = "DatadogAWSIntegrationRole"
+  description = "Role for Datadog AWS Integration"
+  assume_role_policy = "${data.aws_iam_policy_document.datadog_aws_integration_assume_role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "datadog_aws_integration" {
+  role = "${aws_iam_role.datadog_aws_integration.name}"
+  policy_arn = "${aws_iam_policy.datadog_aws_integration.arn}"
+}
